eb46c149306baf8981175883ecb97052077493e5
János Fleck <janos.fleck@gmail.com>
2023-03-30 23:52:08 +0200

    pypi

---
+++ fibo.py
@@ -0,0 +1,21 @@
+x = int(input("How long do you want to?"))
+n1= int(input("what i the beginner number?"))
+n2 = n1+ (n1/2)
+count = 0
+
+# ellenörizzük a szám érvényességét
+if x < 0:
+    x *= -1
+
+# ha 1 jegyüt kér visszaadjuk az elsö elemet7
+elif x  == 1:
+    print("Fibonacci sequence upto",x,":")
+    print(n1)
+else:
+    print("Fibonacci sequence:")
+    while count < x:
+        print(n1)
+        nx=n1+n2
+        n1=n2
+        n2=nx
+        count +=1
--- xx1.py
+++ xx1.py
@@ -1,7 +1,22 @@
-def fib(n):
-    a, b = 0, 1
-    while a < n:
-        print(a, end=' ')
-        a, b = b, a+b
-    print()
-    fib(1000)
+# x = int(input("Please enter an integer: "))
+
+# if x < 0:
+#     x = 0
+#     print('Negative changed to zero')
+# elif x == 0:
+#     print('Zero')
+# elif x == 1:
+#     print('Single')
+# else:
+#     print('More')
+
+# celsius - fahrenheit change
+
+celsius = float(input("Enter temperature in celsius: ")) 
+fahrenheit = (celsius * 1.8) + 32 
+print(str(celsius) + " degree Celsius is equal to " + str(fahrenheit) + " degree Fahrenheit.")
+
+varenheit = float(input("Enter temperature in Farenheit: ")) 
+zelsius = (varenheit - 32) / 1.8
+print(str(varenheit) + " degrees Fahrenheit is equal to " + str(zelsius) + " degrees Celsius")
+
