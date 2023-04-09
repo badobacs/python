1b55206c7fc5b5ccd6c0061d34fe8c38d618234c
János Fleck <janos.fleck@gmail.com>
2023-04-09 22:10:25 +0200

    d2

--- AoC2020/Day2/day2.py
+++ AoC2020/Day2/day2.py
@@ -2,10 +2,36 @@ import csv
 
 with open('input2.csv') as data:
     reader = csv.reader(data, delimiter=' ')
+    validCount = 0
     for row in reader:
         policy, letter, password = row[0], row[1][0], row[2]
-        i = policy.index('-')
-        low = policy[:1]
-        high = policy[i+1:]
-        print(policy, low, high)
         
+        i = policy.index('-')
+        low = int(policy[:i])
+        high = int(policy[i+1:])
+        
+        count = 0
+        for character in password:
+            if character == letter:
+                count += 1
+        if count>= low and count<= high:
+            validCount += 1
+    print(validCount)
+    
+    # part2
+with open('input2.csv') as data:
+    reader = csv.reader(data, delimiter=' ')
+    validCount = 0
+    for row in reader:
+        policy, letter, password = row[0], row[1][0], row[2]
+        
+        i = policy.index('-')
+        low = int(policy[:i])
+        high = int(policy[i+1:])
+        
+        count = 0
+        first = password[low-1] == letter
+        last = password[high-1] == letter
+        if (first and not last) or(last and not first):
+            validCount += 1
+    print(validCount)
