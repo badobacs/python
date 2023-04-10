9a00b3b28b1aadfa11df7598d9469ff474175c36
János Fleck <janos.fleck@gmail.com>
2023-04-10 13:36:32 +0200

    sd

---
+++ 02/allatsag/allatsag.py
@@ -0,0 +1,15 @@
+# N = int(input())
+# eladas=0
+# T=[]
+# for i in range(N):
+#     T.append(int(input()))
+
+# # for j in range(T):
+# #    if T[j] > T[j-1]:
+# #        eladas += 1
+# print(T)
+
+with open('be1.txt') as file:
+    lists=file.readlines()
+    
+print(lists)
